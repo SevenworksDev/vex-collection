4bc81e132e2c3297ba7382ff42d9d4f51b97ec66
Sevenworks <91027492+SevenworksDev@users.noreply.github.com>
2022-10-25 09:55:40 -0500

    Delete replit.nix

--- replit.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.subversion_1_10
-    pkgs.python39Packages.pip
-    pkgs.nodePackages.vscode-langservers-extracted
-    pkgs.nodePackages.typescript-language-server
-  ];
-}
